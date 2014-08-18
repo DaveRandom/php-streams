373718bc94f8b7baa8e967edbf0c5fbdf17bc35b
Chris Wright <github@daverandom.com>
2014-08-18 23:45:10 +0100

    Option handling in StreamServerSocket

--- src/Sockets/StreamServerSocket.php
+++ src/Sockets/StreamServerSocket.php
@@ -12,6 +12,25 @@ class StreamServerSocket extends StreamSocket
      */
     public function __construct($uri, array $options = [])
     {
+        if (!isset($options['socket']['client_class'])) {
+            $options['socket']['client_class'] = StreamPeerSocket::class;
+        }
+
+        foreach ($options as $family => $opts) {
+            foreach ($opts as $option => $value) {
+                $this->setOption($family, $option, $value);
+            }
+        }
+
+        $uriParts = $this->parseURI($uri);
+        if ($uriParts['scheme'] === 'unix' && !isset($uriParts['path'])) {
+            throw new \LogicException('Invalid URI: The unix:// scheme requires a path component');
+        } else if ($uriParts['scheme'] !== 'unix' && !isset($uriParts['host'], $uriParts['port'])) {
+            throw new \LogicException(
+                'Invalid URI: The ' . $uriParts['scheme'] . ':// scheme requires host and port components'
+            );
+        }
+
         $ctx = stream_context_create($options);
 
         $this->socket = stream_socket_server($uri, $errNo, $errStr, STREAM_SERVER_BIND | STREAM_SERVER_LISTEN, $ctx);
@@ -49,8 +68,8 @@ class StreamServerSocket extends StreamSocket
         if ($family === 'socket' && $option === 'client_class') {
             $value = (string)$value;
 
-            if (!is_subclass_of($value, 'StreamPeerSocket')) {
-                throw new \LogicException('Client class must extend StreamPeerSocket');
+            if (!is_subclass_of($value, StreamPeerSocket::class)) {
+                throw new \LogicException('Client class must extend ' . StreamPeerSocket::class);
             }
         }
 
